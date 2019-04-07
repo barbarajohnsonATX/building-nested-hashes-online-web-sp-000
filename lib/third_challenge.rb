dab75cf67eb2c64dbec400260900233e5eb65ea3
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 02:48:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,10 @@ def third_challenge
         :age => "15",
         :status => "alive"
       },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"}, 
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        ]
       }, 
     :capulet => {
       :patriarch => {
@@ -31,7 +34,10 @@ def third_challenge
         :status => "alive"
         
       },
-      :heroine_firends => []
+      :heroine_firends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
       } 
   }
   
